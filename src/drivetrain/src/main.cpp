d9ae5bae61e4c81283f10d3bc769aedaaa758789
Dylan <i532470@FontysICT.nl>
2025-05-03 16:57:47 +0200

    Updated code to use multi-threading for testing

--- src/drivetrain/src/main.cpp
+++ src/drivetrain/src/main.cpp
@@ -1,12 +1,18 @@
+#include <pthread.h>
 #include "ParallexFeedback360.hpp"
-
 
 ParallexFeedback360 *test;
 
-int main() {
-    test = new ParallexFeedback360(18);
+void *test_method(void *arg) {
+    test = new ParallexFeedback360(12);
 
     while (1) {}
+}
+
+int main() {
+    pthread_t test_thread_id;
+    pthread_create(&test_thread_id, NULL, test_method, NULL);
 
     return 0;
+    pthread_exit(NULL);
 }
