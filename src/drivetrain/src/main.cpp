a42538642ec89e4c64f60b433157d4c5954f17b0
Dylan <i532470@FontysICT.nl>
2025-05-04 16:33:52 +0200

    Added time library to prevent system freezing

--- src/drivetrain/src/main.cpp
+++ src/drivetrain/src/main.cpp
@@ -1,4 +1,5 @@
 #include <pthread.h>
+#include <time.h>
 #include "ParallexFeedback360.hpp"
 
 ParallexFeedback360 *test;
@@ -6,13 +7,18 @@ ParallexFeedback360 *test;
 void *test_method(void *arg) {
     test = new ParallexFeedback360(12);
 
-    while (1) {}
+    while (1) {
+        // For ~10ms sleep
+        struct timespec ts = {0, 10 * 1000 * 1000};  // 10 million nanoseconds = 10 ms
+        nanosleep(&ts, NULL);
+    }
+
+    pthread_exit(NULL);
 }
 
 int main() {
     pthread_t test_thread_id;
     pthread_create(&test_thread_id, NULL, test_method, NULL);
-
+    pthread_join(test_thread_id, NULL);
     return 0;
-    pthread_exit(NULL);
 }
