b328a584cfc6a2c6c1da383a7babc7b8c1b28a39
Dylan <i532470@FontysICT.nl>
2025-05-05 15:57:43 +0200

    Changed some values for testing

--- src/drivetrain/src/ParallexFeedback360.cpp
+++ src/drivetrain/src/ParallexFeedback360.cpp
@@ -4,8 +4,8 @@ ParallexFeedback360::ParallexFeedback360(const uint8_t control_pin) :
     control_pin(control_pin) {
     if (wiringPiSetupPinType(WPIPinType::WPI_PIN_PHYS) == WIRINGPI_INVALID_PARAMETER_ERROR) {return;}
     pwmSetRange(2000);
-    pwmSetClock(192);
-    pwmWrite(this->control_pin, 100);
+    pwmSetClock(192); 
+    pwmWrite(this->control_pin, 1280); // Set to 0 to prevent unexpected behavior on startup
     pinMode(this->control_pin, PWM_MS_OUTPUT); 
 }
 
@@ -23,5 +23,6 @@ bool ParallexFeedback360::MoveCounterClockwise(int speed) {
 }
 
 void ParallexFeedback360::Stop(void) {
+    pwmWrite(this->control_pin, 0); 
     return;
 }
